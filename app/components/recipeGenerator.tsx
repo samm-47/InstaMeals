714622dcdb4ff6687912276d0f039e9a15b525be
samm-47 <96743711+samm-47@users.noreply.github.com>
2025-02-28 14:26:27 -0700

    Update recipeGenerator.tsx

--- app/components/recipeGenerator.tsx
+++ app/components/recipeGenerator.tsx
@@ -85,7 +85,7 @@ useEffect(() => {
 
   const sendRecipesToEmail = async () => {
     try {
-      const response = await axios.post('http://127.0.0.1:5000/send_recipes', {
+      const response = await axios.post('https://instameals.onrender.com/send_recipes', {
         email,
         recipes: savedRecipes,
       });
@@ -121,7 +121,7 @@ useEffect(() => {
     };
 
     try {
-      const response = await axios.post<RecipeResponse>('http://127.0.0.1:5000/generate_recipe', recipeRequest);
+      const response = await axios.post<RecipeResponse>('https://instameals.onrender.com/generate_recipe', recipeRequest);
       setRecipe(response.data.recipe);
     } catch (err) {
       console.error('Error generating recipe:', err);
